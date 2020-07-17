120aa07666c550a2c28b321f9e6aa6f171b8bf80
saifudinzezy <saifudin.zezy7@gmail.com>
2020-07-18 00:15:29 +0700

    add icon maps

--- app/src/main/java/com/tugas/akhirtugas/DataLongsor/MapsActivity.java
+++ app/src/main/java/com/tugas/akhirtugas/DataLongsor/MapsActivity.java
@@ -9,6 +9,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -36,7 +37,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-6.900622, 109.664720);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Pekalongan"));
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Pekalongan"))
+                .setIcon(BitmapDescriptorFactory.fromResource(R.drawable.home));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         //mMap.animateCamera(CameraUpdateFactory.newLatLng(sydney));
 
--- app/src/main/java/com/tugas/akhirtugas/DataLongsor/detail/DetailLongsor.java
+++ app/src/main/java/com/tugas/akhirtugas/DataLongsor/detail/DetailLongsor.java
@@ -18,6 +18,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -135,10 +136,10 @@ public class DetailLongsor extends AppCompatActivity implements OnMapReadyCallba
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(Double.parseDouble(data.getLatitude()), Double.parseDouble(data.getLongitude()));
-        mMap.addMarker(new MarkerOptions().position(sydney).title(data.getLokasi()));
+        mMap.addMarker(new MarkerOptions().position(sydney).title(data.getLokasi()))
+                .setIcon(BitmapDescriptorFactory.fromResource(R.drawable.avalanche));
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         //mMap.animateCamera(CameraUpdateFactory.newLatLng(sydney));
 
